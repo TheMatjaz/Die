09d8be0b5c3c470f720ee03b75fbc098d8bfb634
Matjaž Guštin <dev@matjaz.it>
2023-01-11 21:32:07 +0100

    Add support for +- modifiers
    - Parse input with regex instead of with custom logic
    - Improvement of output format
      - Use lowercase `d` for the dice representation
      - Don't show sorted results, only min/max instead

--- README.md
+++ README.md
@@ -1,21 +1,38 @@
 DnD-style dice rolling command line utility
 ===========================================
 
-Python 3.10 or higher required. Just type a list of dice
-rolls in the Die shell, using the format "NdS" where N is the
-amount of dice to roll (1 if unspecified) and S is the 
-amount of sides (6 if unspecified).
+## Usage
+
+Launch `die.py` and type your dice rolls in the `Die` shell.
+
+The format is `NdS+M`, where:
+
+- `N` is the amount of dice to roll (1 if unspecified)
+- `S` are the sides of the die (d20 if unspecified)
+- `M` is the modifier to be added to each roll (0 if unspecified)
+
+For example:
+
+- `1` rolls one 20-sided die
+- `3d8` rolls three 8-sided dice
+- `2d10-3` rolls two 10-sided dice and subtracts three from each result
+- `2+3` rolls two 20-sided die and adds three to each result
+
+Multiple entries are allowed in a single line, e.g. `1d8 3d20+2 6d10`.
+
+## Example
 
 ```bash
 $ python3 die.py
-Die> 4 d2 4d20 1d
-1D4 | 2
-1D2 | 1
-4D20 | Orig: 12, 5, 4, 12
-4D20 | Sort: 4, 5, 12, 12
-4D20 | Sum: 33
-1D6 | 4
+Die> 1 3d8 2d10-3 2+3
+1d20+0	| 20
+3d8+0	| [1, 4, 3] | Min=1 | Max=4 | Sum=8
+2d10+3	| [9, 7] | Min=7 | Max=9 | Sum=16
+2d20+3	| [9, 21] | Min=9 | Max=21 | Sum=30
 Die> exit
 $ 
 ```
 
+## Requirements
+
+Self-contained in `die.py`, requires Python 3.10 or higher, no dependencies.
--- die.py
+++ die.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python3
 # encoding: utf-8
-
+#
 # Copyright © 2023, Matjaž Guštin <dev@matjaz.it> <https://matjaz.it>
 # Released under the BSD 3-Clause License
 
-import secrets
+"""DnD-style dice rolling command line utility."""
+
+import re
+import secrets  # For fair, unpredictable randomness.
+
+VERSION = '1.0.0'
+PATTERN = re.compile(R"([0-9]+)?(?:d([0-9]+))?([+-][0-9]+)?")
+HELP_TEXT = """Type your dice rolls in the format `NdS+M`, where:
+
+- `N` is the amount of dice to roll (1 if unspecified)
+- `S` are the sides of the die (d20 if unspecified)
+- `M` is the modifier to be added to each roll (0 if unspecified)
+
+For example:
+
+- `1` rolls one 20-sided die
+- `3d8` rolls three 8-sided dice
+- `2d10-3` rolls two 10-sided dice and subtracts three from each result
+- `2+3` rolls two 20-sided die and adds three to each result
+
+Multiple entries are allowed in a single line, e.g. `1d8 3d20+2 6d10`.
+"""
 
 
-def repl():
+def repl() -> None:
+    """Runs a REPL which parses user-typed dice-rolling commands and
+    prints the roll results. The ``help`` command provides an explanation
+    on the accepted input format."""
     while True:
         try:
             commands = input("Die> ").strip().lower()
@@ -15,52 +39,57 @@ def repl():
             break
         if commands in ('exit', 'quit'):
             break
-        for command in commands.split():
-            try:
-                amount, sides = parse(command)
-                prompt = f"{amount:d}D{sides:d} | "
-                rolls = roll_dice(amount, sides)
-                if len(rolls) == 1:
-                    print(f"{prompt}{rolls[0]}")
-                else:
-                    print(
-                        f"{prompt}Orig: " + ", ".join(f"{r:d}" for r in rolls))
-                    print(f"{prompt}Sort: " + ", ".join(
-                        f"{r:d}" for r in sorted(rolls)))
-                    print(f"{prompt}Sum: {sum(rolls)}")
-            except ValueError as e:
-                print(f"Bad command: {str(e)}")
-                continue
+        elif commands in ('v', 'version'):
+            print(f"Die, v{VERSION}")
+        elif commands in ('?', 'h', 'help'):
+            print(f"Die, v{VERSION}")
+            print(HELP_TEXT)
+        else:
+            for command in commands.split():
+                try:
+                    amount, sides, modifier = parse_roll_cmd(command)
+                    rolls = roll_dice(amount, sides, modifier)
+                    prompt = f"{amount}d{sides}{modifier:+}\t| "
+                    if len(rolls) == 1:
+                        print(f"{prompt}{rolls[0]}")
+                    else:
+                        print(f"{prompt}{rolls}"
+                              f" | Min={min(rolls)}"
+                              f" | Max={max(rolls)}"
+                              f" | Sum={sum(rolls)}")
+                except ValueError as e:
+                    print(f"Bad command: {str(e)}")
 
 
-def roll_dice(amount: int = 1, sides: int = 6) -> list[int]:
+def roll_dice(amount: int = 1,
+              sides: int = 20,
+              modifier: int = 0) -> list[int]:
+    """Generates ``amount`` (secure) random numbers in [1, ``sides``]
+    and adds ``modifier`` to each of them.
+
+    The amount must be at least 1, the sides must be at least 2."""
     if amount <= 0:
         raise ValueError("Can't roll no dice.")
     if sides <= 1:
         raise ValueError("Can't roll fewer than 2 sides.")
-    return [secrets.randbelow(sides) + 1 for _ in range(amount)]
+    return [secrets.randbelow(sides) + 1 + modifier for _ in range(amount)]
 
 
-def parse(text: str) -> tuple[int, int]:
-    fields = text.strip().lower().split('d')
-    match len(fields):
-        case 1:  # Just the sides, like "6"
-            amount = ''
-            sides = fields[0]
-        case 2:  # "NdS", N=amount, S=sides. N could be empty string.
-            amount = fields[0]
-            sides = fields[1]
-        case _:
-            raise ValueError("Invalid roll text format")
-    if len(amount) == 0:
-        amount = 1
+def parse_roll_cmd(text: str) -> tuple[int, int, int]:
+    """Parses amount (of dice to roll), sides (of the die), and modifier
+    (to add-subtract from each roll) from a text string.
+
+    The format is defined in ``PATTERN`` and explained in ``HELP_TEXT``.
+    Unspecified fields have defaults 1, 20, 0 respectively."""
+    match = PATTERN.fullmatch(text.strip().lower())
+    if match:
+        amount, sides, modifier = match.groups()
+        amount = int(amount or 1)
+        sides = int(sides or 20)
+        modifier = int(modifier or 0)
     else:
-        amount = int(amount)
-    if len(sides) == 0:
-        sides = 6
-    else:
-        sides = int(sides)
-    return amount, sides
+        raise ValueError("Invalid roll command format")
+    return amount, sides, modifier
 
 
 if __name__ == "__main__":
